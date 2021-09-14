3c650ff961642d41db54d6945633aa2416f5c329
Natan Hoppe <nhoppe@gitlab.com>
2021-09-14 17:25:42 +0000

    Add install logic for PostgreSQL extension pg_stat_kcache

--- test/integration/postgresql/extensions/pg_stat_kcache/controls/pg_stat_kcache.rb
+++ test/integration/postgresql/extensions/pg_stat_kcache/controls/pg_stat_kcache.rb
@@ -1,8 +1,8 @@
-control 'PostgreSQL extension pg_wait_sampling' do
+control 'PostgreSQL extension pg_stat_kcache' do
   title 'Check if extension is present'
 
   # NOTE: Update the postgresql version when the default changes
-  describe package('postgresql-12-pg-wait-sampling') do
+  describe package('postgresql-12-pg-stat-kcache') do
     it { should be_installed }
   end
 end
