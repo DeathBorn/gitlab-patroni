4e5412e69745a38e0f7ad1d8112348d2e49d13fa
Natan Hoppe <nhoppe@gitlab.com>
2021-10-07 14:38:50 -0400

    Add recipe to fix SSLError when running Test Kitchen

---
+++ recipes/kitchen_chef_sslerror_fix.rb
@@ -0,0 +1,29 @@
+# Cookbook:: gitlab-patroni
+# Recipe:: kitchen_chef_sslerror_fix
+# License:: MIT
+#
+# Copyright:: 2021, GitLab Inc.
+#
+#  _________________________________
+# /                                 \
+# |     This recipe is intended     |
+# |     for use with Test Kitchen.  |
+# |     DO NOT use in Production.   |
+# \_________________________________/
+#                 !  !
+#                 !  !
+#                 L_ !
+#                / _)!
+#               / /__L
+#         _____/ (____)
+#                (____)
+#         _____  (____)
+#              \_(____)
+#                 !  !
+#                 !  !
+#                 \__/
+
+execute 'Download new certificate file from curl.se for Chef' do
+  command 'rm -rf /opt/chef/embedded/ssl/cert.pem && curl https://curl.se/ca/cacert.pem -o /opt/chef/embedded/ssl/cert.pem'
+  only_if { node['platform_version'].to_i == 16 }
+end
